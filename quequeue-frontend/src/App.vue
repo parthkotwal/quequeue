fb62c80801bf18d910377e07e50b84f128b6179b
Parth Kotwal <parthkotwa07@gmail.com>
2025-06-07 00:12:00 -0700

    vue set up

---
+++ quequeue-frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Que Queue</h1>
+    <p>Welcome! This is a Spotify queue manager</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, sans-serif;
+  text-align: center;
+  padding: 2rem;
+}
+</style>
