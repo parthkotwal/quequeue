16083e40611d687e67afe8a5fab380013f69f96a
Parth Kotwal <parthkotwa07@gmail.com>
2025-09-08 01:50:38 -0700

    retry last

--- quequeue-backend/quequeue_project/settings.py
+++ quequeue-backend/quequeue_project/settings.py
@@ -102,7 +102,6 @@ WSGI_APPLICATION = 'quequeue_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
